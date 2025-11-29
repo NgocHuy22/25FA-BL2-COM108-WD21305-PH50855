51d8bfdf5705209c3bde0a3b6557e576abade649
PH50855 <huynnph50855@gmail.com>
2025-11-29 09:17:36 +0700

    sapxepphantumang

--- CodeTrenLop/CodeTrenLop/Program.c
+++ CodeTrenLop/CodeTrenLop/Program.c
@@ -6,6 +6,29 @@ void kiemTraSoNguyen()
 	printf("\n");
 }
 
+void sapXepPhanTuMang()
+{
+	int integerarray[100];
+	int lenght;
+	printf("Nhap kich thuoc mang : ");
+	printf("\n");
+	scanf_s("%d", &lenght);
+	printf("Nhap du lieu cho mang %d phan tu\n : " , lenght);
+	//int length = sizeof(mangSoNguyen) / sizeof(int);
+	for (int i = 0; i < 0; lenght , i++);
+	{
+		printf("mang[%d] = " , i);
+		scanf_s("%d", &integerarray[i]);
+	}
+	printf(" sap xep tang dan du lieu mang %d phan tu\n",lenght );
+		for (int i = 0; i < 0; lenght - i ; i++);
+
+	{
+		printf("mang[%d] = ", i);
+		scanf_s("%d", &integerarray[i]);
+	}
+}
+
 void lapChucNang(int chonChucNang)
 {
 	int tiepTuc = 1;
@@ -17,7 +40,7 @@ void lapChucNang(int chonChucNang)
 			kiemTraSoNguyen();
 			break;
 		case 2:
-			// ham goi chuc nang 2
+			sapXepPhanTuMang();
 			break;
 		case 3:
 			// ham goi chuc nang 3
@@ -37,63 +60,6 @@ void lapChucNang(int chonChucNang)
 int main()
 {
 	int chonChucNang;
-	do
-	{
-		printf("Menu");
-		printf("\n");
-		printf("1. Kiem Tra So Nguyen");
-		printf("\n");
-		printf("2. TEN chuc nang 2");
-		printf("\n");
-		printf("3. TEN chuc nang 3");
-		printf("\n");
-		printf("0. Thoat");
-		printf("\n");
-		printf("Hay chon chuc nang [0-3]: ");
-		scanf_s("%d", &chonChucNang);
-		lapChucNang(chonChucNang);
-	} while (chonChucNang != 0);
-}
-
-#include <stdio.h>
-
-void kiemTraSoNguyen()
-{
-	printf("Kiem Tra So Nguyen");
-	printf("\n");
-}
-
-void lapChucNang(int chonChucNang)
-{
-	int tiepTuc = 1;
-	while (tiepTuc == 1)
-	{
-		switch (chonChucNang)
-		{
-		case 1:
-			kiemTraSoNguyen();
-			break;
-		case 2:
-			// ham goi chuc nang 2
-			break;
-		case 3:
-			// ham goi chuc nang 3
-			break;
-		default:
-			printf("Chon sai. Chuc nang hop le [0-3]");
-			break;
-		}
-
-		printf("Tiep tuc thuc hien chuc nang nay? [1=Co | 0=Khong]: ");
-		scanf_s("%d", &tiepTuc);
-		system("cls");
-	}
-
-}
-
-int main()
-{
-    int chonChucNang;
 	do
 	{
 		printf("Menu");
